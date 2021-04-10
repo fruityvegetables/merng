a53f14f26939312540337be36f569d83cdf80933
fruityvegetables <gg.troyer@gmail.com>
2021-04-10 05:22:34 -0500

    created react app, and removed some initial files

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Howdy. Welcome to some social media, made with MERN and GraphQL.</h1>
+    </div>
+  );
+}
+
+export default App;
